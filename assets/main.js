cabef97e7f25d2e56d32b345b3dd025bdaa11242
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-05 10:41:57 -0300

    Getting checkbox input value

--- assets/main.js
+++ assets/main.js
@@ -3,9 +3,11 @@ const form = document.getElementById("form-container")
 form.addEventListener("submit", (event) => {
   event.preventDefault()
 
-  let quantity = Number(document.getElementById("quantity").value)
-  let min = Number(document.getElementById("min-range").value)
-  let max = Number(document.getElementById("max-range").value)
+  const quantity = Number(document.getElementById("quantity").value)
+  const min = Number(document.getElementById("min-range").value)
+  const max = Number(document.getElementById("max-range").value)
 
-  console.log(quantity, min, max)
+  const checked = document.getElementById("toggle-switch").checked
+
+  console.log(quantity, min, max, checked)
 })
